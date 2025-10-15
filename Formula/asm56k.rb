19f37724a1132066cbe6a7190360f80d7afa7831
Kåre Andersen <kareandersen@gmail.com>
2025-10-15 11:29:42 +0200

    Initial formula with head only

---
+++ Formula/asm56k.rb
@@ -0,0 +1,14 @@
+class Asm56k < Formula
+  desc "Assembler for DSP56001 with partial DSP56301 support"
+  homepage "https://bitbucket.org/sqward/asm56k"
+  head "https://github.com/kareandersen/asm56k", branch: "main", using: :git
+
+  def install
+    system "make", "CC=clang", "CFLAGS=--std=c89"
+    bin.install "./build/asm56k"
+  end
+
+  test do
+    assert_match "No input file given.", shell_output("#{bin}/asm56k 2>&1", 1)
+  end
+end
---
+++ README.md
@@ -0,0 +1,4 @@
+# Homebrew tap for the asm56k DSP56001 assembler
+
+## See https://github.com/kareandersen/asm56k for more info
+
